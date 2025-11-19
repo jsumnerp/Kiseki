364498985f7b9ec36bd9b43c9983d67a1fedbbd5
jsumnerp <44463202+jsumnerp@users.noreply.github.com>
2025-11-19 19:20:29 +0000

    setup supabase otp password auth (#13)

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,10 +1,23 @@
 import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
 
-export const Header = () => {
+export interface HeaderProps {
+  isLoggedIn: boolean;
+  onLogout?: () => void;
+}
+
+export const Header = ({ isLoggedIn, onLogout }: HeaderProps) => {
   return (
     <header className="w-full py-4 px-6 flex items-center justify-between mb-8 border-b-2 border-muted-foreground">
       <h1 className="text-2xl font-bold">Kiseki</h1>
-      <ModeToggle />
+      <div className="flex gap-4">
+        {isLoggedIn && (
+          <Button onClick={onLogout} variant="destructive">
+            Log Out
+          </Button>
+        )}
+        <ModeToggle />
+      </div>
     </header>
   );
 };
